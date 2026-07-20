35c9c469160686e7d4eb5c54965bb2cd45918288
unknown <chimaobiu@yahoo.com>
2026-07-20 09:07:19 +0100

    fix prisma issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,10 @@
 // prisma.config.ts
-import 'dotenv/config'  // This loads .env file
+import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-  },
-  db: {
-    providers: {
-      postgresql: {
-        url: process.env.DATABASE_URL,  // Now this will work
-      },
-    },
   },
 })
